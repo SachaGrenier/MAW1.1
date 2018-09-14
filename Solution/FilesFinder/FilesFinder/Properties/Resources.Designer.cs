25f4477b82644c19d2a63dbadaa595da73c20134
XSchwab <xavier.schwab@cpnv.ch>
2018-09-14 10:19:56 +0200

    Code update

--- Solution/FilesFinder/FilesFinder/Properties/Resources.Designer.cs
+++ Solution/FilesFinder/FilesFinder/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FileFinder.Properties
+namespace FilesFinder.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace FileFinder.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FileFinder.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FilesFinder.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
