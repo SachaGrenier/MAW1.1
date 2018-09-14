b2266aa3e561b29980b821987822d6d8eca531a5
SCHWAB Xavier <Xavier.SCHWAB@cpnv.ch>
2018-09-14 09:06:51 +0200

    Creation solution

---
+++ Solution/FileFinder/FileFinder/Model/FileDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+namespace ProjetRechercheTest.Model
+{
+    public class FileDetails
+    {
+        public string name { get; set; }
+        public string filename { get; set; }
+        public string Path { get; set; }
+    }
+}
